0aabeab0a0aaf6d0cf2affa51fdd544b98c70fe2
Thiago <thiagodelgado.dev@gmail.com>
2023-11-15 15:23:36 -0300

    add readme and document api

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -13,16 +13,23 @@ const router = express.Router();
 
 /**
  * @openapi
+ * tags:
+ *   name: Workouts
+ *   description: Endpoints related to Crossfit workouts.
+ */
+
+/**
+ * @openapi
  * /api/v1/workouts:
  *   get:
- *     tags:
- *       - Workouts
+ *     summary: Get all workouts.
+ *     tags: [Workouts]
  *     parameters:
  *       - in: query
  *         name: mode
  *         schema:
  *           type: string
- *         description: The mode of a workout
+ *         description: The mode of a workout.
  *     responses:
  *       200:
  *         description: OK
@@ -55,12 +62,290 @@ const router = express.Router();
  *                       type: string
  *                       example: "Some error message"
  */
-router
-  .get("/", getAllWorkouts)
-  .get("/:workoutId", getOneWorkout)
-  .get("/:workoutId/record", getRecordForWorkout)
-  .post("/", createNewWorkout)
-  .patch("/:workoutId", updateOneWorkout)
-  .delete("/:workoutId", deleteOneWorkout);
+router.get("/", getAllWorkouts);
+
+/**
+ * @openapi
+ * /api/v1/workouts/{workoutId}:
+ *   get:
+ *     summary: Get details of a specific workout.
+ *     tags: [Workouts]
+ *     parameters:
+ *       - in: path
+ *         name: workoutId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the workout.
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   $ref: "#/components/schemas/Workout"
+ *       400:
+ *         description: BAD REQUEST
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Parameter ':workoutId' can not be empty"
+ *       404:
+ *         description: NOT FOUND
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Workout not found"
+ */
+router.get("/:workoutId", getOneWorkout);
+
+/**
+ * @openapi
+ * /api/v1/workouts/{workoutId}/record:
+ *   get:
+ *     summary: Get the record for a specific workout.
+ *     tags: [Workouts]
+ *     parameters:
+ *       - in: path
+ *         name: workoutId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the workout.
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   $ref: "#/components/schemas/Record"
+ *       400:
+ *         description: BAD REQUEST
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Parameter ':workoutId' can not be empty"
+ *       404:
+ *         description: NOT FOUND
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Record not found for the workout"
+ */
+router.get("/:workoutId/record", getRecordForWorkout);
+
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   post:
+ *     summary: Create a new workout.
+ *     tags: [Workouts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/NewWorkout"
+ *     responses:
+ *       201:
+ *         description: CREATED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   $ref: "#/components/schemas/Workout"
+ *       400:
+ *         description: BAD REQUEST
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "One of the following keys is missing or is empty in request body: 'name', 'mode', 'equipment', 'exercises', 'trainerTips'"
+ */
+router.post("/", createNewWorkout);
+
+/**
+ * @openapi
+ * /api/v1/workouts/{workoutId}:
+ *   patch:
+ *     summary: Update details of a specific workout.
+ *     tags: [Workouts]
+ *     parameters:
+ *       - in: path
+ *         name: workoutId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the workout.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/UpdateWorkout"
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   $ref: "#/components/schemas/Workout"
+ *       400:
+ *         description: BAD REQUEST
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Parameter ':workoutId' can not be empty"
+ *       404:
+ *         description: NOT FOUND
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Workout not found"
+ */
+router.patch("/:workoutId", updateOneWorkout);
+
+/**
+ * @openapi
+ * /api/v1/workouts/{workoutId}:
+ *   delete:
+ *     summary: Delete a specific workout.
+ *     tags: [Workouts]
+ *     parameters:
+ *       - in: path
+ *         name: workoutId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the workout.
+ *     responses:
+ *       204:
+ *         description: NO CONTENT
+ *       400:
+ *         description: BAD REQUEST
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Parameter ':workoutId' can not be empty"
+ *       404:
+ *         description: NOT FOUND
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Workout not found"
+ */
+router.delete("/:workoutId", deleteOneWorkout);
 
 module.exports = router;
